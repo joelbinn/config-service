08cf3878eb7dabb93d712b3eeebd9728ecd46130
Joel Binnquist <joel.binnquist@gmail.com>
2023-05-01 19:39:53 +0200

    Kägg till config-service

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.11"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.6"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
